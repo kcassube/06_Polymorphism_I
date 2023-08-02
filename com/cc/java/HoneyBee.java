274b26099fab016acce707f4db84f38297d9d0d0
kcassube <79251426+kcassube@users.noreply.github.com>
2023-08-02 09:38:07 +0200

    01_Class_Stuff

---
+++ com/cc/java/HoneyBee.java
@@ -0,0 +1,13 @@
+package com.cc.java;
+
+public class HoneyBee {
+
+    public String doYourJob() {
+        return "Im a HoneyBee";
+    }
+
+    public String fly() {
+        return "I can Fly";
+    }
+    
+}
