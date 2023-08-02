5c62597e8a0f96269d6677aae69126ae4b438e4e
Peter <schmidt.multimedia@gmail.com>
2023-08-02 09:29:35 +0200

    1. Iteration Super/Subklassen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,25 @@ public class App {
     
     public static void main(String[] args) {
      
+        Queen queen = new Queen();
+        Worker worker = new Worker();
+        Drone drone = new Drone();
+
+        output(queen.doYourJob());
+        output(worker.doYourJob());
+        output(drone.doYourJob());
+
+        output("---------------------");
+
+        output(queen.fly());
+        output(worker.fly());
+        output(drone.fly());
+
+
+
+
+
+
         
     }
 
